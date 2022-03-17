8b458ff2f85a393eaab89acd4c8f5605fcc4d248
matovu-farid <matovu90@gmail.com>
2022-03-17 19:54:43 +0300

    Add hero design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Hero from './pages/Hero';
 
 function App() {
   return (
-    <div>Hello world</div>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+    </Routes>
   );
 }
 
