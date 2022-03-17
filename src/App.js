644e2ed618f48e7e96f4c03dd95c8160417375ab
matovu-farid <matovu90@gmail.com>
2022-03-17 20:22:49 +0300

    Add divider design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Hero from './pages/Hero';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Hero />} />
+      <Route path="/" element={<Homepage />} />
     </Routes>
   );
 }
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,9 @@
+import Hero from '../../app/component/Hero';
+
+const Homepage = () => (
+  <div>
+    <Hero />
+  </div>
+);
+
+export default Homepage;
