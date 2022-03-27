0153bf7c0d37f807fae7550d4f8e8fe70f9398d4
matovu-farid <matovu90@gmail.com>
2022-03-27 03:49:08 +0300

    Update footer styles

--- src/Hooks/useData.js
+++ src/Hooks/useData.js
@@ -57,6 +57,24 @@ const useData = () => {
       github: '',
 
     },
+    {
+      id: '3',
+      name: 'Name goes here',
+      description: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+    ullamco laboris nisi Ut aliquip ex ea commodo consequat. 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+    ullamco laboris nisi.`,
+      tags: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+      image: Image,
+      live: '',
+      github: '',
+
+    },
   ];
   const [data] = useState(array);
   return data;
