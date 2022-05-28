5eeb57e6d868ff9ba4266cd2e08584ae960fca49
matovu-farid <matovu90@gmail.com>
2022-05-28 15:23:01 +0300

    Add github link

--- src/component/Projects/index.js
+++ src/component/Projects/index.js
@@ -4,13 +4,15 @@ import style from './style.module.scss';
 
 function Item({ item }) {
   return (
-    <div>
+    <div className='container mx-auto' >
       <h2>{item.name}</h2>
       <p>{item.description}</p>
       <img className={style.projectimg} src={item.image} alt="" />
-      <button class="bg-blue-500 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-        Github
-      </button>
+      <div className='w-full flex flex-col justify-center'>
+        <a href={item.github} target='_blank' className="mx-auto cursor-pointer bg-gray-900 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+          Github
+        </a>
+      </div>
     </div>
   );
 }
