81d0d65d1661b65bfc00c777a2f82802f0cbd48e
matovu-farid <matovu90@gmail.com>
2022-07-12 19:20:44 +0300

    Add type animations

--- src/component/Hero/index.js
+++ src/component/Hero/index.js
@@ -1,26 +1,24 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
 import propTypes from 'prop-types';
+import TypeAnimation from 'react-type-animation';
 import style from './style.module.scss';
 
 const Hero = ({ showMenu }) => (
   <section className={`${style.section} section md:relative md:top-20`}>
-
     <div id="hero" className={`${style.hero} `}>
       <div className={`content ${style.content}`}>
-
         <div className={style.menu}>
           <GiHamburgerMenu onClick={showMenu} />
         </div>
-        <div className="flex flex-col justify-center align-middle">
-
-          <h1 className="heading lg:text-8xl">
-            Hi! am,
-            <br />
-            {' '}
-            Farid Matovu
-          </h1>
+        <div className="flex flex-col justify-center align-middle w-96">
+          <TypeAnimation
+            className="heading lg:text-8xl"
+            cursor={false}
+            sequence={['Hi! am', 1000, 'Farid Matovu', 10000]}
+            repeat={Infinity}
+            wrapper="h1"
+          />
         </div>
-
       </div>
     </div>
   </section>
